634a86833502d650fa1f1d79c84fd3c97f12b07c
Samuel Prada <jspradac@gmail.com>
2018-11-16 23:48:41 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,24 @@
 def consolidate_cart(cart)
-  # code here
+  hash = {}
+  cart.each do |product|
+    product.each do |name, details|
+      if !((hash.keys).include?(name))
+        details[:count] = cart.count(product)
+        hash[name] = details
+      end 
+    end 
+  end 
+  hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  hash = cart 
+  coupons.each do |coupon|
+    coupon.each do |key, value|
+      if (cart.keys).include?(value)
+        hash[value + "W/COUPON"] = {price: coupon[:cost], clearance: true, count: (coupon[:num])/(cart[value][:count])}
+        hash[value] = {
+      
 end
 
 def apply_clearance(cart)

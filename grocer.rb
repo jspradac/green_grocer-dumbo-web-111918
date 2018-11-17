632cf227ece57ff85ef216d4cc00b44fd64e98cc
Samuel Prada <jspradac@gmail.com>
2018-11-17 00:50:04 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -18,7 +18,7 @@ def apply_coupons(cart, coupons)
     coupon.each do |key, value|
       if (con_cart.keys).include?(value)
         if (hash.keys).include?(value + " W/COUPON")
-          hash[value + " W/COUPON"][:count] += 1 
+          hash[value + " W/COUPON"][:count] += 0
         else 
           hash[value + " W/COUPON"] = {price: coupon[:cost], clearance: con_cart[value][:clearance], count: (con_cart[value][:count])/(coupon[:num])}
           hash[value] = {price: con_cart[value][:price], clearance: con_cart[value][:clearance], count: (con_cart[value][:count])%(coupon[:num])}
